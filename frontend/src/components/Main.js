c9bbf6fff648ca53218e1cbd3a9d06536bc8d4fd
tomkf <thomaskeithferris@gmail.com>
2019-07-29 04:47:19 +0000

    frontend scaffold

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Chat from "components/Chat.js";
+
+class AppComponent extends React.Component {
+  render() {
+    return <Chat />;
+  }
+}
+
+AppComponent.defaultProps = {};
+
+export default AppComponent;
