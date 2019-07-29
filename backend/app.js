01bbd69a13c8424f33d2367db8a4b2b4ee8921f7
tomkf <thomaskeithferris@gmail.com>
2019-07-29 04:28:50 +0000

    basic socket server and packages

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+server.listen(3000);
+
+io.on("connection", function(socket) {
+  // console.log('user connected')
+
+  socket.on("disconnect", function() {
+    // console.log('user disconnected')
+  });
+
+  socket.on("chat", function(data) {
+    // console.log('message: ' + data)
+    io.emit("chat", data);
+  });
+});
